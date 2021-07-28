9a961f0f5f5b901413c5850e94d71e5f3f13ee8f
divyagariya12 <divyagariya11@gmail.com>
2021-07-29 03:03:24 +0530

    initial project setup

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView,View,Text} from 'react-native';
+
+export default () => {
+  return (
+      <SafeAreaView>
+        <View>
+          <Text>Hello App!</Text>
+        </View>
+      </SafeAreaView>
+  );
+};
